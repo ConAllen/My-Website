7356df0077445cf6828eb4ca34fe2a240873733a
Your Name <you@example.com>
2018-02-06 20:34:24 +0000

    about me page

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,6 +17,31 @@ $('.js--wp-1').waypoint(function(direction) {
     offset: '100%'
 });
 
+$(function() {
+
+	var $bars = $( ".skillbar-bar" ),
+		total = $bars.length,
+		index = -1,
+		timer = null,
+		select = function( i ) {
+			var $bar = $bars.eq( i );
+			$bar.css( "width", $bar.parent().data( "percent" ) );
+			$bar.parent().addClass( "complete" );
+		},
+		action = function() {
+			index++;
+			if( index == total - 1 ) {
+				clearTimeout( timer );
+			}
+			select( index );
+			setTimeout( action, 500 );
+
+		};
+
+
+	timer = setTimeout( action, 500 );
+
+});
 
 
 });
